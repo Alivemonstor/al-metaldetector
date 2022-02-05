7b95c7763e8b559fbf4c3f75a90bd6967862f008
Jeremy <68263976+ansljeremy@users.noreply.github.com>
2022-02-05 17:51:30 -0500

    Added detection area to config

--- client.lua
+++ client.lua
@@ -4,7 +4,7 @@ local Player = QBCore.Functions.GetPlayerData(src)
 
 RegisterNetEvent('al-metaldetector:detect')
 AddEventHandler('al-metaldetector:detect', function()
-    if #(GetEntityCoords(PlayerPedId()) - vector3(2805.56, 4746.63, 46.45)) < 1000.0 then
+    if #(GetEntityCoords(PlayerPedId()) - Config.DetectionArea) < 1000.0 then
         TriggerEvent('animations:client:EmoteCommandStart', {"kneel3"})
         QBCore.Functions.Progressbar('metalennit', 'Searching The Area', math.random(5000, 10000), false, true, {
             disableMovement = true,
--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 Config = {}
 
+Config.DetectionArea = vector3(2805.56, 4746.63, 46.45)
+
 Config.MetalLoot = {
      ['Tier1'] = {
         'plastic',
